2980455c655bfb30c26a49bb8807fce022cc05bb
CeMaGo <cecilemarleen@gmail.com>
2025-03-05 15:33:23 +0100

    beauty fix

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -7,11 +7,16 @@ interface CardProps {
 
 const CardComponent: React.FC<CardProps> = ({ card }) => {
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg p-4">
+    <div className="max-w-sm rounded overflow-hidden shadow-lg p-4 m-2">
       <div className="px-6 py-4">
         <div className="font-bold text-xl mb-2">{card.name}</div>
-        <p className="text-gray-700 text-base">{card.meaning_up}</p>
+        <p className="text-gray-500 text-base">
+          <strong>Upright:</strong> {card.meaning_up}
+        </p>
         <p className="text-gray-700 text-base">{card.meaning_rev}</p>
+        <p className="text-gray-500 text-base">
+          <strong>Reversed:</strong> {card.meaning_rev}
+        </p>
         <p className="text-gray-700 text-base">{card.desc}</p>
       </div>
     </div>
