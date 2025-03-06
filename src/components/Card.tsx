d1dbccbfa8065efca8b5481f26ad9b65163e7f3e
CeMaGo <cecilemarleen@gmail.com>
2025-03-06 14:48:34 +0100

    getting the image dynamic

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -7,8 +7,6 @@ interface CardProps {
   isLoading: boolean;
 }
 
-const imageSrc = "/images/Cards/Major Arcana/01_TheMagician.png";
-
 const CardComponent: React.FC<CardProps> = ({ card, isLoading }) => {
   const [showMeanings, setShowMeanings] = useState(false);
   const [isReversed, setIsReversed] = useState(Math.random() < 0.5);
@@ -29,6 +27,25 @@ const CardComponent: React.FC<CardProps> = ({ card, isLoading }) => {
     return null;
   }
 
+  const getImagePath = (card: TarotCard): string => {
+    if (card.name_short.startsWith("ar")) {
+      // Major Arcana
+      return `/images/Cards/Major Arcana/${card.name_short}.png`;
+    } else {
+      // Minor Arcana
+      const suit = card.name_short.substring(0, 2); // Get the suit (cu, wa, sw, pe)
+      const suitFolder = {
+        cu: "Cups-Hearts-Water",
+        wa: "Wands-Clubs-Fire",
+        pe: "Pentacles-Diamonds-Earth",
+        sw: "Swords-Spades-Air",
+      }[suit];
+      return `/images/Cards/Minor Arcana/Suits/${suitFolder}/${card.name_short}.png`;
+    }
+  };
+
+  const imageSrc = getImagePath(card);
+
   return (
     <div
       className={`max-w-sm rounded overflow-hidden shadow-lg p-4 m-2 ${
@@ -44,8 +61,11 @@ const CardComponent: React.FC<CardProps> = ({ card, isLoading }) => {
         >
           {isReversed ? "Reversed" : "Upright"}
         </p>
-        <img src={imageSrc} alt={card.name} className="mt-4 w-full" />
-        {/* Card Image */}
+        <img
+          src={imageSrc}
+          alt={card.name}
+          className="mt-4 w-full bg-opacity-100"
+        />
         <button
           className="text-sky-400 hover:text-sky-300 mt-2"
           onClick={toggleMeanings}
